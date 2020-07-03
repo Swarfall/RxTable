068688ec6b391249ab2be932570b898bd3a8e65a
Savitsky Viacheslav <49763132+Swarfall@users.noreply.github.com>
2020-07-03 12:16:41 +0300

    first commit

---
+++ RxTable/RxTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxTable
+//
+//  Created by Вячеслав on 03.07.2020.
+//  Copyright © 2020 ViacheslavSavitsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
